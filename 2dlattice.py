bca2bedc013cfdae3cb5ed0193a461212bd5201a
Zubin <goldenterminator_1993@hotmail.com>
2015-04-28 03:12:25 +0200

    Start of the algorithm

--- 2dlattice.py
+++ 2dlattice.py
@@ -41,23 +41,58 @@ plt.show()
 pi=(math.pi)
 sqrt=(math.sqrt)
 
-dens=np.zeros(9) #initial condition zero velocity everywhere??
-Angles=np.zeros(9)
-e=np.zeros((2,9),dtype=float)
 m=1
 dt=1
+tau=1 # What should this be???
+T=100
 
-dens[0]=3E10 #Is this a good pick?
+
+
+
+dens=np.zeros((n,9),dtype=float) #initial condition zero velocity everywhere??
+Angles=np.zeros(9)
+e=np.zeros((2,9),dtype=float)
+Totdens=np.zeros((n,1))
+AvV_sq=np.zeros((n,1))
+Ext=np.ones((1,9))
+Tdens=np.zeros((n,9,T))
+denspos=np.zeros((n,2,9))
+
+
+
+dens[:,0]=3E10 #Is this a good pick?
 
 w1=np.array([4, 1, 1, 1, 1, 1, 1, 1, 1],dtype=float)
 w2=np.array([9, 36, 9, 36, 9, 36, 9, 36, 9],dtype=float)
 w=np.divide(w1,w2)
 
-Angles[1:(len(dens))]=np.arange(0,2*pi,pi/4)
+Angles[1:(dens.shape[1])]=np.arange(0,2*pi,pi/4)
 
-e[:,1:len(dens)]=np.array([[np.cos(Angles[1:(len(dens))]),np.sin(Angles[1:(len(dens))])]]) #unit vectors for each of the 9 directions.
+e[:,1:dens.shape[1]]=np.array([[np.cos(Angles[1:(dens.shape[1])]),np.sin(Angles[1:(dens.shape[1])])]]) #unit vectors for each of the 9 directions.
 
 V=np.array([0, 1/dt, sqrt(2)/dt, 1/dt, sqrt(2)/dt, 1/dt, sqrt(2)/dt, 1/dt, sqrt(2)/dt])
 
-AvV=np.dot(V*dens,np.transpose(e))
-dens_eq=w*sum(dens)*(1+3*np.dot(AvV,e)+9/2*np.dot(AvV,e)**2-3/2*np.dot(AvV,np.transpose(AvV)))
+for k in range(0,len(V)):
+    
+    denspos[:,:,k]=R #optimize this step.
+
+Totdens[:,0]=dens.sum(axis=1)
+
+for j in range(0,len(V)):
+  for i in range(0,n):
+    for t in range(0,T):
+         Tdens[i+dt,j,t]=dens[i+dt,j]
+         
+         denspos[i,:,j]=denspos[i,:,j] + dt*np.transpose(e[:,j])
+        
+         AvV=np.dot(V*dens,np.transpose(e))/Totdens
+         
+         AvV_sq[:,0]=(AvV**2).sum(axis=1)
+         
+         dens_eq=w*Totdens*(1+3*np.dot(AvV,e)+9/2*np.dot(AvV,e)**2-3/2*np.dot(AvV_sq,Ext))
+         
+         dens[i+dt,j]=dens[i,j]-1/tau*(dens[i,j]-dens_eq[i,j])
+         
+         
+
+
