b0256813ca313e749fd9000ac854327d0822f0ae
Zubin <goldenterminator_1993@hotmail.com>
2015-04-28 16:31:26 +0200

    moderate progress

--- 2dlattice.py
+++ 2dlattice.py
@@ -8,7 +8,7 @@ Created on Sun Apr 26 17:23:50 2015
 import numpy as np
 import math
 import matplotlib.pyplot as plt
-#T=300
+
 
 
 rows=6
@@ -44,54 +44,76 @@ sqrt=(math.sqrt)
 m=1
 dt=1
 tau=1 # What should this be???
-T=100
+T=10
 
 
 
 
-dens=np.zeros((n,9),dtype=float) #initial condition zero velocity everywhere??
+dens=np.zeros((n,9,T+dt),dtype=float) #initial condition zero velocity everywhere??
 Angles=np.zeros(9)
 e=np.zeros((2,9),dtype=float)
-Totdens=np.zeros((n,1))
-AvV_sq=np.zeros((n,1))
+Totdens=np.zeros((n,1,T+1))
+AvV_sq=np.zeros((n,T))
 Ext=np.ones((1,9))
 Tdens=np.zeros((n,9,T))
 denspos=np.zeros((n,2,9))
+dens_eq=np.zeros((n,9,T))
+AvV=np.zeros((n,2,T))
+H=np.zeros((n,1))
 
 
 
-dens[:,0]=3E10 #Is this a good pick?
+dens[:,0]=3 #Is this a good pick?
 
 w1=np.array([4, 1, 1, 1, 1, 1, 1, 1, 1],dtype=float)
 w2=np.array([9, 36, 9, 36, 9, 36, 9, 36, 9],dtype=float)
 w=np.divide(w1,w2)
 
-Angles[1:(dens.shape[1])]=np.arange(0,2*pi,pi/4)
-
-e[:,1:dens.shape[1]]=np.array([[np.cos(Angles[1:(dens.shape[1])]),np.sin(Angles[1:(dens.shape[1])])]]) #unit vectors for each of the 9 directions.
-
 V=np.array([0, 1/dt, sqrt(2)/dt, 1/dt, sqrt(2)/dt, 1/dt, sqrt(2)/dt, 1/dt, sqrt(2)/dt])
+
+Angles[1:len(V)]=np.arange(0,2*pi,pi/4)
+
+e[:,1:len(V)]=np.array([[np.cos(Angles[1:len(V)]),np.sin(Angles[1:len(V)])]]) #unit vectors for each of the 9 directions.
+
+
 
 for k in range(0,len(V)):
     
     denspos[:,:,k]=R #optimize this step.
 
-Totdens[:,0]=dens.sum(axis=1)
 
 for j in range(0,len(V)):
   for i in range(0,n):
     for t in range(0,T):
-         Tdens[i+dt,j,t]=dens[i+dt,j]
+#         Tdens[i+dt,j,t]=dens[i+dt,j]
          
-         denspos[i,:,j]=denspos[i,:,j] + dt*np.transpose(e[:,j])
+          
+          if denspos[i,1,j]==1:
+              
+              if j>=6:                  
+               denspos[i,:,j]=denspos[i,:,j] + dt*np.transpose(e[:,j])
+               denspos[i,:,(j-4)]=denspos[i,:,j]
+             
+          elif denspos[i,1,j]==(rows-1):
+              
+               if 1<j<5:
+                denspos[i,:,j]=denspos[i,:,j] + dt*np.transpose(e[:,j])
+                denspos[i,:,(j+4)]=denspos[i,:,j]
+                
+          else: denspos[i,:,j]=denspos[i,:,j] + dt*np.transpose(e[:,j])
+                
+            
         
-         AvV=np.dot(V*dens,np.transpose(e))/Totdens
+          Totdens[:,0,:]=dens.sum(axis=1)
+
+          AvV[:,:,t]=np.dot(V*dens[:,:,t],np.transpose(e))/Totdens[:,:,t]
+          AvV_sq[:,t]=(AvV[:,:,t]**2).sum(axis=1)
+          
+          H[:,0]=AvV_sq[:,t]
+
+          dens_eq[:,:,t]=w*Totdens[:,:,t]*(1+3*np.dot(AvV[:,:,t],e)+9/2*np.dot(AvV[:,:,t],e)**2-3/2*np.dot(H,Ext))
          
-         AvV_sq[:,0]=(AvV**2).sum(axis=1)
-         
-         dens_eq=w*Totdens*(1+3*np.dot(AvV,e)+9/2*np.dot(AvV,e)**2-3/2*np.dot(AvV_sq,Ext))
-         
-         dens[i+dt,j]=dens[i,j]-1/tau*(dens[i,j]-dens_eq[i,j])
+          dens[:,:,t+dt]=dens[:,:,t]-1/tau*(dens[:,:,t]-dens_eq[:,:,t])
          
          
 
