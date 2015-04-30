77b31d625fe09b9e1a3f6f7172ec5ff0372dea51
Zubin <goldenterminator_1993@hotmail.com>
2015-04-30 17:29:13 +0200

    progres

--- 2dlattice.py
+++ 2dlattice.py
@@ -1,10 +1,7 @@
 import numpy as np
 import math
 import matplotlib.pyplot as plt
-rows=6
-columns=6
-i=0
-n=rows**2 #assume 1 particle at each site? Lattice spacing of 1.
+
 def initLattice(nx,ny):
     i=0
     R=np.zeros((nx*ny,2),dtype=float)
@@ -18,16 +15,13 @@ def initLattice(nx,ny):
     ax.set_xlabel('X Label')
     ax.set_ylabel('Y Label')
     plt.show()
+    return R
 
 #### MODEL PARAMETERS##########################################################
 nx = 50 ; ny = 20 ; q = 9 ; dt = 1 ; tau = 1 ; m = 1 ; maxiter = 10 ; P0=1
 e = np.array([[0,1,1,0,-1,-1,-1,0,1], [0,0,1,1,1,0,-1,-1,-1]]) # unit vectors
 weight = np.array([[4./9] , [1./36] , [1./9] , [1./36] , [1./9] , [1./36] , [1./9] , [1./36] , [1./9] ])
 ##############################################################################
-i1 = np.array([1,2,8]) #unknown at left wall
-i2 = np.array([0,3,7]) #unknown vertical
-i3 = np.array([4,5,6]) #unknown at right wall
-
 def equilibrium (rho,u):
     for i in range(q):
         eu = np.dot(e.T[i],u.transpose(1,0,2))
@@ -39,179 +33,33 @@ def equilibrium (rho,u):
 u0 = np.zeros((2,nx,ny)) # Initial condition
 denseq = equilibrium(1.0,u0) ; densin = denseq.copy()
 
-#for time in range(maxiter):
-rho = np.sum(densin,axis=0)
-u = np.dot(e,densin.transpose(1,0,2))/rho    
+mask=np.zeros((nx,ny))
+mask[:,(0,ny-1)]=1, mask = mask==1
+#index = np.transpose(np.nonzero(mask))
 
-rho[0] = 3*P0*np.ones((ny))
-u[0,0,:]= 1- (np.sum(densin[i2,0,:],axis=0) +2*np.sum(densin[i3,0,:],axis=0))/rho[0]
-
-densin[1,0,:]= denseq[1,0,:] + densin[5,0,:]-denseq[5,0,:]
-densin[2,0,:]= 0.5*(rho[0]+denseq[5,0,:]-denseq[1,0,:]-densin[0,0,:])-denseq[7,0,:]-denseq[5,0,:]-denseq[4,0,:]-denseq[3,0,:]
-densin[8,0,:]= 0.5*(rho[0]+denseq[5,0,:]-denseq[1,0,:]-densin[0,0,:])-denseq[6,0,:]-denseq[5,0,:]
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-<<<<<<< HEAD
-dens=np.zeros((columns,rows,q),dtype=float) #initial condition zero velocity everywhere??
-Angles=np.zeros(q)
-e=np.zeros((2,q),dtype=float)
-Totdens=np.zeros((columns,rows))
-AvV_sq=np.zeros((columns,rows))
-Ext=np.ones((1,q))
-#Tdens=np.zeros((n,9,T))
-#denspos=np.zeros((columns,rows,q))
-dens_eq=np.zeros((columns,rows,q))
-Av=np.zeros((columns,rows,2))
-AvV=np.zeros((columns,rows,2))
-H=np.zeros((columns,rows))
-
-
-
-dens[:,:,3]=3 #Is this a good pick?
-=======
-
-
-
->>>>>>> d006984159c1d1539a10b83c22e12bb5ac937e7a
-
-
-
-
-
-
-<<<<<<< HEAD
-w = np.array([[4./9] , [1./36] , [1./9] , [1./36] , [1./9] , [1./36] , [1./9] , [1./36] , [1./9] ])
-
-
-R=initLattice(n,rows) 
-
-#for k in range(0,q):
-#    
-#    denspos[:,:,k]=R #optimize this step.
-
-
-
-for t in range(0,T):
-    for j in range(0,q):
-        for i in range(0,columns):
-              for k in range(0,rows):
-#         Tdens[i+dt,j,t]=dens[i+dt,j]
-         
-                dens[i+e[0,j],k+e[1,j],j] = dens[i,k,j]
-          
-                if (dens[:,1,j]==np.ones(columns)).all():
-              
-                   if j>=6:                  
-                     dens[i+e[0,j],k+e[1,j],j] = dens[i,k,j]
-                     dens[:,:,(j-4)]=dens[:,:,j]
-             
-                elif (dens[:,1,j]==(rows-1)*np.ones(columns)).all():
-              
-                  if 1<j<5:
-                    dens[i+e[0,j],k+e[1,j],j] = dens[i,k,j]
-                    dens[:,:,(j+4)]=dens[:,:,j]
-                
-          
-                
+for time in range(maxiter):
+    rho = np.sum(densin,axis=0)
+    u = np.dot(e,densin.transpose(1,0,2))/rho    
+    
+    
+    for j in range(0,q): #Streaming w/ Bounce-Back
+           densin[j,:,:] = np.roll(np.roll(densin[j,:,:],e[0,j],axis=0),e[1,j],axis=1)
+           
+           densin[j,mask] = densin[j+4,mask]
+#           u[j,index[0],index[1]]= -1* u[j,index[0],index[1]]
+                  
+#               if j>=6:                  
+##                 densin[j,:,:] = np.roll(np.roll(densin[j,:,:],e[0,j],axis=0),e[1,j],axis=2)
+#                 densin[(j-4),:,0]=densin[j,:,:]
+#         
+#           elif (densin[j,:,1]==(ny-1)*np.ones(nx)).all():
+#           
+#              if 1<j<5:
+##                densin[j,:,:] = np.roll(np.roll(densin[j,:,:],e[0,j],axis=0),e[1,j],axis=1)
+#                densin[(j+4),:,ny]=densin[j,:,:]
             
-        
-                  Totdens=dens.sum(axis=2)
-          
-                  Av=dens[i,k,j]*e[:,j]/Totdens[i,k]
-                  AvV[i,k,:]=Av+AvV[i,k,:]
-#          AvV[:,:,0]=np.dot(dens,np.transpose(e))/Totdens
-#          AvV[:,:,1]=np.dot(dens,np.transpose(e))/Totdens
-                  AvV_sq[:,:]=(AvV[:,:,:]**2).sum(axis=2)
-          
-#          H[:,0]=AvV_sq[:,:]
-
-#          dens_eq[:,:,:]=np.transpose(w)*Totdens[:,:]*(1+3*np.dot(AvV[:,:,:],e)+9/2*np.dot(AvV[:,:,:],e)**2-3/2*np.dot(AvV_sq,Ext))
-#         
-#          dens[:,:,:]=dens[:,:,t]-1/tau*(dens[:,:,t]-dens_eq[:,:,t])
-         
-         
-=======
-##########Algorithm#########################
-#pi=(math.pi)
-#sqrt=(math.sqrt)
-#
-#
-#
-#dens=np.zeros((columns,rows,q),dtype=float) #initial condition zero velocity everywhere??
-#Angles=np.zeros(q)
-#e=np.zeros((2,q),dtype=float)
-#Totdens=np.zeros((columns,rows))
-#AvV_sq=np.zeros((columns,rows,T))
-#Ext=np.ones((1,q))
-##Tdens=np.zeros((n,9,T))
-##denspos=np.zeros((columns,rows,q))
-#dens_eq=np.zeros((columns,rows,q))
-#AvV=np.zeros((columns,rows,T))
-#H=np.zeros((n,1))
-#
-#
-#
-#dens[:,0]=3 #Is this a good pick?
-#
-#
-#
-##V=np.array([0, 1/dt, sqrt(2)/dt, 1/dt, sqrt(2)/dt, 1/dt, sqrt(2)/dt, 1/dt, sqrt(2)/dt])
-#
-#Angles[1:q]=np.arange(0,2*pi,pi/4)
-# 
-#
-##for k in range(0,q):
-##    
-##    denspos[:,:,k]=R #optimize this step.
-#
-#
-#for j in range(0,q):
-#    for t in range(0,T):
-##         Tdens[i+dt,j,t]=dens[i+dt,j]
-#         
-#          dens[:,:,j]=dens[e[0,j],e[1,j],j] 
-#          
-#          if (dens[:,1,j]==np.ones(columns)).all():
-#              
-#              if j>=6:                  
-#               dens[:,:,j]=dens[e[0,j],e[1,j],j] 
-#               dens[:,:,(j-4)]=dens[:,:,j]
-#             
-#          elif (dens[:,1,j]==(rows-1)*np.ones(columns)).all():
-#              
-#               if 1<j<5:
-#                dens[:,:,j]=dens[e[0,j],e[1,j],j] 
-#                dens[:,:,(j+4)]=dens[:,:,j]
-#                
-#          
-#                
-#            
-#        
-#          Totdens=dens.sum(axis=2)
-#
-#          AvV[:,:,t]=np.dot(dens[:,:,j],np.transpose(e))/Totdens
-#          AvV_sq[:,t]=(AvV[:,:,t]**2).sum(axis=1)
-#          
-#          H[:,0]=AvV_sq[:,t]
-#
-#          dens_eq[:,:,t]=np.transpose(w)*Totdens[:,:,t]*(1+3*np.dot(AvV[:,:,t],e)+9/2*np.dot(AvV[:,:,t],e)**2-3/2*np.dot(H,Ext))
-#         
-#          dens[:,:,t+dt]=dens[:,:,t]-1/tau*(dens[:,:,t]-dens_eq[:,:,t])
-#         
-#         
->>>>>>> d006984159c1d1539a10b83c22e12bb5ac937e7a
-
-
+    denseq = equilibrium(rho,u)
+    u[0]+=0.05 
+    
+    for j in range (q): #Relaxation
+        densin[j,:,:] = (1-1/tau)*densin[j,:,:] +denseq[j,:,:]
