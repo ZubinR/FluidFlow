a4e2f9d3529c2f389c0305dc5d96d9d65586caaf
misrbechan <misrbechan@gmail.com>
2015-05-02 11:16:35 +0200

    Forgot factor 1/tau at relaxation

--- 2dlattice.py
+++ 2dlattice.py
@@ -17,26 +17,32 @@ def initLattice(nx,ny):
     return R
 
 #### MODEL PARAMETERS##########################################################
-nx = 50 ; ny = 20 ; q = 9 ; dt = 1 ; tau = 1 ; m = 1 ; maxiter = 10
+nx = 50 ; ny = 20 ; q = 9 ; dt = 1 ; tau = 0.5 ; m = 1 ; maxiter = 10
 e = np.array([[0,1,1,0,-1,-1,-1,0,1], [0,0,1,1,1,0,-1,-1,-1]]) # unit vectors
-weight = np.array([[4./9] , [1./36] , [1./9] , [1./36] , [1./9] , [1./36] , [1./9] , [1./36] , [1./9] ])
-##############################################################################
+weight = np.array([[4./9], [1./36], [1./9], [1./36], [1./9], [1./36], [1./9],
+                   [1./36], [1./9]])
+###############################################################################
 def equilibrium (rho,u):
     denseq = np.zeros((q,nx,ny))
     eu = np.dot(e.T,u.transpose(1,0,2))
     u2 = u[0]**2+u[1]**2
     for i in range(q):                      
-        denseq[i,:,:] = rho * weight[i] * (1. + 3. * eu[i] + 0.5*9*eu[i]**2 - 1.5*u2)
+        denseq[i,:,:] = rho * weight[i] * (1. + 3. * eu[i] + 0.5*9*eu[i]**2 - 
+                        1.5*u2)
     return denseq
 
-u0 = np.zeros((2,nx,ny));ut0=u0[0] # Initial condition
-denseq = equilibrium(1.0,u0) ; densin = denseq.copy()
 
-mask=np.zeros((nx,ny));mask[:,(0,ny-1)]=1; mask = mask==1
+u0 = np.zeros((2,nx,ny));ut0=u0[0] # Initial condition
+denseq = equilibrium(1.0,u0) 
+densin = denseq.copy()
+mask=np.zeros((nx,ny))
+mask[:,(0,ny-1)]=1 
+mask = mask==1
 
 for time in range(maxiter):    
     for j in range(q): #Streaming w/ Bounce-Back
-        densin[j,:,:] = np.roll(np.roll(densin[j,:,:],e[0,j],axis=0),e[1,j],axis=1)
+        densin[j,:,:] = np.roll(np.roll(densin[j,:,:],e[0,j],axis=0),e[1,j],
+                        axis=1)
         if 0<j<=4:
             densin[j,mask] = densin[j+4,mask]
         elif j>4:
@@ -44,14 +50,15 @@ for time in range(maxiter):
 
     rho = np.sum(densin,axis=0)
     u = np.dot(e,densin.transpose(1,0,2))/rho   
-    u[0]+=0.05       
+    u[0]+=0.5       
     denseq = equilibrium(rho,u)    
     for j in range (q): #Relaxation
-        densin[j,:,:] = (1-1/tau)*densin[j,:,:] +denseq[j,:,:]
-#Steady State condition        
+        densin[j,:,:] = (1-1/tau)*densin[j,:,:] + denseq[j,:,:]/tau
+##Steady State condition        
 #    u1=u[0]
 #    if np.sum(ut1-usum0)/np.sum(ut1)<10:
 #        print (maxiter)
 #        break
 #    else: 
 #        ut0=ut1
+plt.plot(u[0,0,:],np.arange(ny))
