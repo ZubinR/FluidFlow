234656981c89008c4ee4353e95d7de2ceeb6f7d7
Zubin <goldenterminator_1993@hotmail.com>
2015-05-10 03:16:03 +0200

    Added Velocity magnitude color plot to profile and animation of the magnitude over time

--- LBM_Poiseuille.py
+++ LBM_Poiseuille.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy.optimize as opt
 from pylab import *
+from matplotlib import animation
 #### MODEL PARAMETERS##########################################################
 nx = 50 ; ny = 20 ; q = 9 ; tau = 1.85 ; maxiter = 100; du = 0.005
 obx= nx/2; oby = ny/2  #coordinates of bottom left rectangular obstacle point
@@ -35,6 +36,8 @@ u0 = np.zeros((2,nx,ny))
 rho0=np.ones((nx,ny))
 denseq = equilibrium(rho0,u0) 
 densin = denseq.copy()
+Ux=np.zeros((maxiter,nx,ny))
+Uy=np.zeros((maxiter,nx,ny))
     
 mask = np.ones((nx,ny),dtype=bool)
 mask[:,[0,-1]]=False
@@ -65,23 +68,57 @@ for time in range(maxiter):
     rho = np.sum(densin,axis=0)
     u = np.dot(e.T,densin.transpose(1,0,2))/rho  
     u[0,mask]+=du    
+    Ux[time,:,:]=u[0,:,:]
+    Uy[time,:,:]=u[1,:,:]
 #    print(rho[1,1]) # Checks density conservation
     denseq = equilibrium(rho,u)
  
     for j in range (q): #Relaxation
         densin[j,mask] = (1-1/tau)*densin[j,mask] + denseq[j,mask]/tau
 #
-#U=u[0,-1,:]
-#y=np.arange(ny)
-#a,cov=opt.curve_fit(Velx,y,U,0.0,None)    
-#plt.plot(U,y, 'r+', Velx(y,a),y) 
-#plt.xlabel('Horizontal Velocity') ; plt.ylabel('y') ;plt.title( 'Poisseuile Flow')
-#curve=curvature(U)
+        
+#####Plotting velocity profiles#####        
+U=u[0,obx,:]
+Re =np.sum(u)*ny/(nx*ny*visc)
+y=np.arange(ny)
+a,cov=opt.curve_fit(Velx,y,U,0.0,None) 
+plt.figure()
+plt.plot(U,y, 'r+', Velx(y,a),y) 
+plt.xlabel('Horizontal Velocity') ; plt.ylabel('y') ;plt.title( 'Poiseuille Flow, '"Re=%g"%Re)
+curve=curvature(U)
+plt.show()
 #
+Vinx=np.transpose(Ux[-1,:,:]) 
+Viny=np.transpose(Uy[-1,:,:])       
 v = np.transpose(u)
-Q = quiver(v[1:-1,:,0], v[1:-1,:,1])
-l,r,b,t = axis()
-dx, dy = r-l, t-b
-axis([l-0.05*dx, r+0.05*dx, b-0.05*dy, t+0.05*dy])
+fig, ax = plt.subplots(1,1)
+plt.imshow(v[1:-1,:,0]**2+v[1:-1,:,1]**2)
+plt.colorbar()
+Q = ax.quiver(Vinx[1:-1,:], Viny[1:-1,:])
+ax.set_xlim(0, nx-1)
+ax.set_ylim(0, ny-2)
+
+plt.show()
+
+##########Animation##########
+fig, ax = plt.subplots(1,1)
+im=ax.imshow(Vinx[1:-1,:]**2+Viny[1:-1,:]**2)
+
+#
+def animate(i,im,Ux,Uy):
+    
+    Vinx=np.transpose(Ux[i,:,:])
+    Viny=np.transpose(Uy[i,:,:])
+    im.set_array(Vinx[1:-1,:]**2+Viny[1:-1,:]**2)
+    im.autoscale()
+    
+    
+#    Q.set_UVC(Vinx,Viny)
+    
+    return im
+    
+anim = animation.FuncAnimation(fig, animate, np.arange(maxiter),fargs=(im,Ux, Uy),
+                               interval=100, blit=False, repeat=True)    
 title('velocity profile rectangular obstacle')
-show()  
+show() 
+ 
